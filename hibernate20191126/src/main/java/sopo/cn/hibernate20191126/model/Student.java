214a67b1c573fd6b48ae6d9775aed0db34a56d93
hyokjoy lee <hpharry@qq.com>
2019-12-04 10:50:36 +0800

    learn HIBERNATE

--- hibernate20191126/src/main/java/sopo/cn/hibernate20191126/model/Student.java
+++ hibernate20191126/src/main/java/sopo/cn/hibernate20191126/model/Student.java
@@ -3,42 +3,82 @@ package sopo.cn.hibernate20191126.model;
 import java.sql.Date;
 
 public class Student {
+	public Student(int id, String name, String sex, int age, Date date) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.sex = sex;
+		this.age = age;
+		this.date = date;
+	}
+
+	public Student() {
+		super();
+	}
+
+	public Student(String name, String sex, int age, Date date) {
+		super();
+		this.name = name;
+		this.sex = sex;
+		this.age = age;
+		this.date = date;
+	}
+
 	private int id;
 	private String name;
 	private String sex;
 	private int age;
 	private Date date;
+	private Teacher teacher;
+
+	public Teacher getTeacher() {
+		return teacher;
+	}
+
+	public void setTeacher(Teacher teacher) {
+		this.teacher = teacher;
+	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getSex() {
 		return sex;
 	}
+
 	public void setSex(String sex) {
 		this.sex = sex;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
+
 	public Date getDate() {
 		return date;
 	}
+
 	public void setDate(Date date) {
 		this.date = date;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Student [id=" + id + ", name=" + name + ", sex=" + sex + ", age=" + age + ", date=" + date + "]";
